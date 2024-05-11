80b9a76ac3d8f0c48b502bf090e62f5f411ed769
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-05-11 16:51:20 +0630

    Create excel model, jobs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        for ($i = 0; $i < 90000; $i++) {
+            User::create([
+                'name' => fake()->name(),
+                'email' => fake()->email(),
+                'password' => fake()->password()
+            ]);
+        }
     }
 }
